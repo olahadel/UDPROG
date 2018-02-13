530680edaa20b48e496187d21aa5eb2b5f343d8b
olahadel <36456978+olahadel@users.noreply.github.com>
2018-02-13 21:37:47 +0100

    Create csere_szorzassal.cpp

---
+++ csere_szorzassal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a=0, b=0;
+cout<<"Adja meg 'a' erteket:";
+cin>>a;
+cout<<"Adja meg 'b' erteket:";
+cin>>b;
+a=a*b;
+b=a/b;
+a=a/b;
+cout<<endl<<"Csere:"<<endl<<"a:"<<a<<endl<<"b:"<<b;
+}
