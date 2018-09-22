9cd8ab1923d3654adac79ae3c0e99e3ff159e18a
olahadel <36456978+olahadel@users.noreply.github.com>
2018-09-22 11:52:58 +0200

    Add files via upload

--- prog2/2. hét/szulo_gyerek.cpp
+++ prog2/2. hét/szulo_gyerek.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
-
 using namespace std;
 
 class Szulo{
-    void fuggveny(){};
+    public:
+        virtual void fuggveny(){
+            cout<<"szulo"<<endl;
+        };
 };
 
 class Gyerek: public Szulo{
-    
+    public:
+        void fuggveny() {
+            cout<<"gyerek"<<endl;
+        }
+        void ujfuggveny() {
+            cout<<"ujfuggveny"<<endl;
+        }
 };
 
-int main(){
+int main() {
+    Szulo *szulo;
+    szulo = new Szulo();
+
+    Gyerek *gyerek;
+    gyerek = new Gyerek();
+
+    Szulo *ujgyerek;
+    ujgyerek = new Gyerek();
+
+    Gyerek *ujszulo;
+    //ujszulo = new Szulo(); //Gyerek osztály több mindent tartalmaz a Szülőnél
+
+    szulo->fuggveny();
+    gyerek->fuggveny();
+    ujgyerek->fuggveny();
+    //ujgyerek->ujfuggveny(); //a Szülő classnak nincs ujfuggveny nevű függvénye
     
     return 0;
 }
