7050b637ae53c8adc55ac49311abcdd6861cefac
olahadel <36456978+olahadel@users.noreply.github.com>
2018-10-07 16:59:41 +0200

    Add files via upload

--- prog2/1. hét/PolarGenerator/PolarGenerator.cpp
+++ prog2/1. hét/PolarGenerator/PolarGenerator.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <time.h>
+#include <ctime>
 #include <cstdlib>
 
 using namespace std;
@@ -9,46 +9,45 @@ class PolarGenerator {
     public:
         PolarGenerator(){
             nincsTarolt = true;
+            srand(time(NULL));
         }
-        /*PolarGenerator(bool a){
-            bool a;
-            a = true;
-            return a;
-        }
-        */
-        double kovetkezo(bool nincsTarolt, double tarolt) {
+
+        double kovetkezo() {
             if (nincsTarolt) {
                 double u1, u2, v1, v2, w;
                 do {
-                    u1 = rand();
-                    u2 = rand();
+                    u1=rand() / (RAND_MAX+1.0);
+                    u2=rand() / (RAND_MAX+1.0);
                     v1 = 2 * u1 - 1;
                     v2 = 2 * u2 - 1;
                     w = v1 * v1 + v2 * v2;
                 } while (w > 1);
+
                 double r = sqrt((-2 * log(w)) / w);
                 tarolt = r * v2;
                 nincsTarolt = !nincsTarolt;
                 return r * v1;
+
             } else {
                 nincsTarolt = !nincsTarolt;
+                return tarolt;
             }
-            return tarolt;
         }
 
         private:
-            bool nincsTarolt= true;
+            bool nincsTarolt;
             double tarolt;
 };
 
-
 int main(){
-    srand(time(NULL));
-    bool a=true;
-    double b=0;
-    PolarGenerator g; // = new PolarGenerator;
-    for (int i = 0; i < 10; ++i) {
-        cout<<g.kovetkezo(a, b);
+
+    PolarGenerator g;
+
+    //cout<<g.kovetkezo();
+
+    for (int i = 0; i < 10; i++) {
+        cout<<g.kovetkezo()<<endl;
     }
 
+    return 0;
 }
