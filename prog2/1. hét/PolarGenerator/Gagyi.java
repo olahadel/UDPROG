dd4729a97897ad70dc3776cea3e91f12cef92a6f
olahadel <36456978+olahadel@users.noreply.github.com>
2018-10-07 17:45:44 +0200

    Add files via upload

--- prog2/1. hét/PolarGenerator/Gagyi.java
+++ prog2/1. hét/PolarGenerator/Gagyi.java
@@ -1,18 +1,26 @@
 public class Gagyi {
 
-    public static void main(String[] args) {
-        /*Integer x;
-        x = new Integer();
-        Integer t;
-        t = new Integer();
-        x=128;
-        t=128;
-        
-        while (x <= t && x >= t && t != x){
-            System.out.println(x);*/
-        
-        }
-    }
-    
+	public static void main(String[] args) {
+		
+		Integer x = 128;
+		Integer t = 128;
+		
+		if(x <= t && x >= t && x != t) {
+			System.out.println("1. x = " + x + " t = " + t);
+		}	
+		
+		x = 4;
+		t = 4;
+		
+		if(x <= t && x >= t && x != t) {
+			System.out.println("2. x = " + x + " t = " + t);
+		}
+		
+		x = new Integer(4);
+		t = new Integer(4);
+		
+		if(x <= t && x >= t && x != t) {
+			System.out.println("3. Belép x = " + x + " t = " + t);
+		}
+	}
 }
-
