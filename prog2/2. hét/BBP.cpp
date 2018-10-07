5be876ef4118823a9dda5c29bdc83940d2dd7331
olahadel <36456978+olahadel@users.noreply.github.com>
2018-10-07 21:55:03 +0200

    Add files via upload

--- prog2/2. hét/BBP.cpp
+++ prog2/2. hét/BBP.cpp
@@ -1,78 +1,82 @@
 #include <iostream>
 #include <cmath>
+#include <time.h>
+
 using namespace std;
 
-extern string d16PiHexaJegyek; 
+// 16^n mod k
 
-void BBP(int d) {  
-    double d16Pi = 0.0d; //szam
+long n16modk(int n, int k) {
+    long r= 1;
+    int t= 1;
 
-    double d16S1t = d16Sj(d, 1); //a
-    double d16S4t = d16Sj(d, 4); //b
-    double d16S5t = d16Sj(d, 5); //c
-    double d16S6t = d16Sj(d, 6); //d
+    while (t<=n)
+        t *= 2;
 
-    d16Pi = 4.0d * d16S1t - 2.0d * d16S4t - d16S5t - d16S6t; //szam=4*a-2*b-c-d
-
-    d16Pi = d16Pi - floor(d16Pi); // szam= szam-(nala kisebb egesz szam--őt hatarolo kisebb egesz szam)
-
-    StringBuffer sb = new StringBuffer();
-
-    char hexaJegyek[] = {'A', 'B', 'C', 'D', 'E', 'F'};
-
-    while (d16Pi != 0.0d) {
-        int jegy = (int) floor(16.0d * d16Pi); //jegy= 16*szam also egesz hataroloja intre konvertalva
-
-        if (jegy < 10)
-            sb.append(jegy);
-        else
-            sb.append(hexaJegyek[jegy - 10]);
-
-        d16Pi = (16.0d * d16Pi) - floor(16.0d * d16Pi); // szam=szamnak a törtrésze
-    }
-    d16PiHexaJegyek = sb.toString();
-}
-
-double d16Sj(int d, int j) {
-        
-    double d16Sj = 0.0d;
-
-    for (int k = 0; k <= d; ++k)
-        d16Sj += (double) n16modk(d - k, 8 * k + j) / (double) (8 * k + j);
-
-    return d16Sj - floor(d16Sj);
-}
-
-long int n16modk(int n, int k) {
-
-    int t = 1;
-    while (t <= n)
-        t = t * 2;
-
-    long r = 1;
-
-    while (true) {
-        if (n >= t) {
-            r = (16 * r) % k;
-            n = n - t;
+    for (;;) {
+        if (n>=t) {
+            r = (16*r) % k;
+            n = n-t;
         }
 
-        t = t / 2;
+        t = t/2;
 
-        if (t < 1)
+        if (t<1)
             break;
 
-        r = (r * r) % k;
+        r = (r*r) % k;
     }
     return r;
 }
 
-string toString() {
-    return d16PiHexaJegyek;
+double d16Sj(int d, int j) {
+    double d16Sj = 0.0;
+    int k;
+
+    for (k = 0; k <= d; ++k)
+        d16Sj += (double) n16modk(d - k, 8 * k + j) / (double) (8 * k + j);
+
+    /*
+       for(k=d+1; k<=2*d; ++k)
+       d16Sj += pow(16.0, d-k) / (double)(8*k + j);
+     */
+
+    return d16Sj - floor(d16Sj);
 }
-    
-int main(){
-    cout<<"10^6 db számjegy hexadecimálisan: " << new BBP(1000000)) << endl;
-    cout<<"10^7 db számjegy hexadecimálisan: " << new BBP(10000000) << endl;
-    cout<<"10^8 db számjegy hexadecimálisan: " << new BBP(100000000);
+
+// {16^d Pi} = {4*{16^d S1} - 2*{16^d S4} - {16^d S5} - {16^d S6}}
+
+main() {
+
+    double d16Pi = 0.0;
+
+    double d16S1t = 0.0;
+    double d16S4t = 0.0;
+    double d16S5t = 0.0;
+    double d16S6t = 0.0;
+
+    int jegy;
+
+    clock_t delta = clock();
+
+    //for(int d=1000000; d<1000001; ++d) {    //10^6.szj kiírása
+    //for(int d=10000000; d<10000001; ++d) {    //10^7.szj kiírása
+    for(int d=100000000; d<100000001; ++d) {    //10^8.szj kiírása
+        d16Pi = 0.0;
+
+        d16S1t = d16Sj(d, 1);
+        d16S4t = d16Sj(d, 4);
+        d16S5t = d16Sj(d, 5);
+        d16S6t = d16Sj(d, 6);
+
+        d16Pi = 4.0 * d16S1t - 2.0 * d16S4t - d16S5t - d16S6t;
+
+        d16Pi = d16Pi - floor(d16Pi);
+
+        jegy = (int) floor(16.0 * d16Pi);
+    }
+
+    cout<<jegy<<endl;
+    delta = clock() - delta;
+    cout<< (double) delta/ CLOCKS_PER_SEC;
 }
